3476ae1e64915c5c9e403cf6defc12a0676adf83
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-27 13:30:16 +0530

    Updates

---
+++ precache-manifest.215b8d0b942956e0144a23ee83068ea8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af822d4758f03a5f9062c1bdef53b3e",
+    "url": "/Metronome/index.html"
+  },
+  {
+    "revision": "bf187d777d1ed3ea9f6e",
+    "url": "/Metronome/static/css/main.2c593057.chunk.css"
+  },
+  {
+    "revision": "62065005b4acade5c3af",
+    "url": "/Metronome/static/js/2.5ff25694.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Metronome/static/js/2.5ff25694.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf187d777d1ed3ea9f6e",
+    "url": "/Metronome/static/js/main.774f8712.chunk.js"
+  },
+  {
+    "revision": "e46c1a1cf0d446b59f9f",
+    "url": "/Metronome/static/js/runtime-main.61ac1222.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/Metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/Metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
